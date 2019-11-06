bdb9f3c9b555f268b63a0feebb2e14854d457d31
Anton <anton.revta@gmail.com>
2019-11-06 17:14:17 +0300

    Updates

---
+++ precache-manifest.c4d63794752d15410a268eaba4a93484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5945c1b1e559860fc32c3d41dba9000b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e83fcf77bece3c88515a",
+    "url": "/portfolio/static/css/main.025ba7eb.chunk.css"
+  },
+  {
+    "revision": "f5178074766144bd0696",
+    "url": "/portfolio/static/js/2.e1f7d9ab.chunk.js"
+  },
+  {
+    "revision": "e83fcf77bece3c88515a",
+    "url": "/portfolio/static/js/main.b7805096.chunk.js"
+  },
+  {
+    "revision": "51c7d4a7aac2907f15b9",
+    "url": "/portfolio/static/js/runtime-main.d4086776.js"
+  }
+]);
