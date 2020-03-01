2ad8cf38a1d656f37683c5cfaa66f1da961ddeee
Anton <anton.revta@gmail.com>
2020-03-01 13:43:18 +0300

    small aligning of main info block

--- src/components/Projects/Project/Project.jsx
+++ src/components/Projects/Project/Project.jsx
@@ -2,42 +2,30 @@ import React from 'react';
 import style from './../Projects.module.css';
 import Button from "../../Button/Button";
 
-class Project extends React.Component {
-    /*state = {
-        isFocus: false
-    }
-    makeIsFocusTrue = () => {
-        this.setState({isFocus: true})
-    }
-    makeIsFocusFalse = () => {
-        this.setState({isFocus: false})
-    }*/
-    ProjectImgStyle = {
-        backgroundImage: 'url(' + this.props.img + ')',
+const Project = (props) => {
+
+    const ProjectImgStyle = {
+        backgroundImage: 'url(' + props.img + ')',
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat'
     }
-    render = () => {
         return (
             <div className={style.containerProject}>
-                <div style={this.ProjectImgStyle} className={style.projectImg} onMouseEnter={this.makeIsFocusTrue}
-                     onMouseLeave={this.makeIsFocusFalse}>
+                <div style={ProjectImgStyle} className={style.projectImg}>
                     <div className={style.projectButton}>
                         <Button buttonName='cмотреть'/>
                     </div>
                 </div>
                 <div className={style.projectInfo}>
                 <span className={style.projectName}>
-                {this.props.name}
+                {props.name}
                 </span>
                     <span className={style.projectDesc} >
-                {this.props.desc}
+                {props.desc}
                 </span>
                 </div>
             </div>
         )
-    }
-
 }
 export default Project;
