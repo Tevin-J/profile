ae350906eed8cbe97355dec061401c88a32894b9
Anton <anton.revta@gmail.com>
2020-05-15 12:52:30 +0300

    add todolist in projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,15 +5,17 @@ import Title from "../Title/Title";
 import socialNetwork from '../../assets/img/social-network.PNG';
 import balcony from '../../assets/img/balcony.PNG'
 import cats from '../../assets/img/cats.jpg'
+import todolist from '../../assets/img/todolist.jpg'
 import Fade from 'react-reveal/Fade';
 
 const Projects = () => {
     const projectData = [
         {id: 1, name: 'Социальная сеть', desc: 'Большой учебный проект социальной сети на React. В проекте применены как' +
                 ' функциональные, так и классовые компоненты. State management осуществляется посредством Redux, а так' +
-                ' же хуков. Взаимодействие с сервером при помощи библиотеки axios. Архитектура приложения четко разделена на три блока: UI, BLL, DAL.', img: socialNetwork, url: 'https://tevin-j.github.io/React-app'},
-        {id: 2, name: 'Cats app', desc: 'Небольшое приложение о кошках, написанное в связке React-Redux-TypeScript. State management реализован посредством хуков', img: cats, url: 'https://tevin-j.github.io/cat-test/'},
-        {id: 3, name: 'Landing page на чистом JS', desc: 'Проведена работа с табами, модальными окнами, формами, таймером. Весь функционал реализован на чистом JavaScript.', img: balcony, url: 'https://github.com/Tevin-J/balconySite'}
+                ' же хуков. Взаимодействие с сервером при помощи библиотеки axios. Архитектура приложения четко разделена на три блока: UI, BLL, DAL.', img: socialNetwork, url: 'https://github.com/Tevin-J/React-app'},
+        {id: 2, name: 'Cats app', desc: 'Небольшое приложение о кошках, написанное в связке React-Redux-TypeScript. State management реализован посредством хуков', img: cats, url: 'https://github.com/Tevin-J/cat-test'},
+        {id: 3, name: 'Landing page на чистом JS', desc: 'Проведена работа с табами, модальными окнами, формами, таймером. Весь функционал реализован на чистом JavaScript.', img: balcony, url: 'https://github.com/Tevin-J/balconySite'},
+        {id: 4, name: 'TodoList', desc: 'Реализован с помощью React, Redux, TypeScript, есть взаимодействие с сервером. Написан на классовых компонентах с применением методов жизненного цикла.', img: todolist, url: 'https://github.com/Tevin-J/todoList'},
     ];
     const projectsElements = projectData.map(title => <Project name={title.name} desc={title.desc} img={title.img}
                                                         key={title.id} url={title.url}/>)
